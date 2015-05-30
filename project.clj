e199ea4065d4ff5c89747d8681aa35eed9b039f6
Keisuke Fukuda <keisukefukuda@gmail.com>
2015-05-30 17:40:35 +0900

    improved math formulas using cinfix library

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject euler "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :description "My solutions to Project Euler"
+  :url "https://github.com/keisukefukuda/euler"
+  :license {:name "MIT License"
+            :url "https://github.com/keisukefukuda/euler/blob/master/LICENSE"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/math.numeric-tower "0.0.4"]
-                 [chiara "0.2.0"]])
+                 [cinfix "0.2.0"]])
--- src/euler/prob58.clj
+++ src/euler/prob58.clj
@@ -1,33 +1,27 @@
+;; http://freak-da.hatenablog.com/entry/2015/05/30/162610
 (ns euler.prob58
   (:require [clojure.math.numeric-tower :as math]
             [euler.numbers :refer :all]
-            [chiara]))
+            [cinfix.core :refer ($=)]))
 
 (defn count-primes [nums]
   (count (filter is-prime? nums)))
 
 (defn D [^long n] ;; 4n^2 + 4n + 1 = (2n+1)^2
   (if (zero? n) 1
-      (+ (* 4 (* n n))
-         (* 4 n)
-         1)))
+      ($= "4*n*n + 4*n + 1")))
 
 (defn C [^long n] ;; Cn = 4n^2 = 2n + 1
   (if (zero? n) 1
-      (+ (* 4 (* n n))
-         (* 2 n)
-         1)))
+      ($= "4*n*n + 2*n + 1")))
 
 (defn B [^long n] ;; 4n^2 + 1
   (if (zero? n) 1
-      (+ (* 4 (* n n))
-         1)))
+      ($= "4*n*n + 1")))
 
 (defn A [^long n] ;; 4n^2 - 2n + 1
   (if (zero? n) 1
-      (+ (* 4 (* n n))
-         (* -2 n)
-         1)))
+      ($= "4*n*n - 2*n + 1")))
 
 (defn answer [^double threshold]
   (loop [n 1 nums 5 nprms 0]
@@ -41,12 +35,13 @@
                (< ratio threshold))
         (do
           (println n a b c d " [" nprms "/" nums "] " ratio)
-          (println "n = " n ", answer = " (+ (* 2 n) 1)))
+          (let [answer (+ (* 2 n) 1)]
+            (println "n = " n ", answer = " answer)
+            answer))
         (recur (inc n) (+ nums 4) nprms)))))
 
 (defn run [& args]
-  (time
-   (answer (if (empty? args)
-             0.10
-             (Double/parseDouble (first args))))))
+  (answer (if (empty? args)
+            0.10
+            (Double/parseDouble (first args)))))
 
